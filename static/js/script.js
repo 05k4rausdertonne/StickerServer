6a963ad25bc4408b4012b2cd4193b9b53ff4270d
05k4rausdertonne <oskarflath@gmail.com>
2024-08-18 17:30:56 +0200

    add input form

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 // static/js/script.js
 document.addEventListener('DOMContentLoaded', function() {
-    alert('Welcome to the Home Page!');
+    var form = document.getElementById("lform");
+
+    document.getElementById("lsubmit").addEventListener("click", function () {
+        console.log(`printing label with text "${form.elements.ltext.value}"`)
+        // do stuff
+    });
+
 });
