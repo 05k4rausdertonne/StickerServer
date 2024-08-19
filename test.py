88a9588ccdbcb8ae74ef6f9e57d76cbb0b3db6d6
05k4rausdertonne <oskarflath@gmail.com>
2024-08-19 21:08:55 +0200

    test

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ vendor_id = 0x28e9
 product_id = 0x0289
 
 # Resize image to fit the printer width
-img = Image.open("test.jpg").convert("L")
+# img = Image.open("test.jpg").convert("L")
 
 
 p = Usb(vendor_id, product_id, 0, timeout=5000)
