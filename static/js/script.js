e29fd48eafbd4e59b7ee5cbbfb89ff9315c4307f
05k4rausdertonne <oskarflath@gmail.com>
2024-08-20 00:23:08 +0200

    test

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,8 @@
 // static/js/script.js
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById("lsubmit").addEventListener("click", function () {
+    document.getElementById("lbutton").addEventListener("click", function () {
+        
         console.log(`printing label with text "${form.elements.ltext.value}"`)
         // do stuff
     });
