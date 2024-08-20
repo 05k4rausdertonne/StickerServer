b0101231f7432dadcf288389d7cf8d0d1abae319
05k4rausdertonne <oskarflath@gmail.com>
2024-08-20 20:33:27 +0200

    new try

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,16 @@
 // static/js/script.js
 document.addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById("lbutton").addEventListener("click", function () {
+    let sliders = document.getElementsByClassName("rangeslider");
+
+    sliders.forEach(slider=> {
+        let output = document.getElementsById(`${slider.id}output`)
+        slider.addEventListener("input", function  () {
+            output.value = slider.value;
+        });
+    });
+
+    document.getElementsById("lbutton").addEventListener("click", function () {
         
         console.log(`printing label with text "${form.elements.ltext.value}"`)
         // do stuff
