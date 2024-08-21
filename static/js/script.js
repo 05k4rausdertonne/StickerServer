0c1d20b1e63929f3dada10824652580a0714bebe
05k4rausdertonne <oskarflath@gmail.com>
2024-08-21 12:01:21 +0200

    fix bug

--- static/js/script.js
+++ static/js/script.js
@@ -5,28 +5,28 @@ function removeFE0F(str) {
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    let sliders = document.getElementsByClassName("rangeslider");
+    let sliders = document.getElementsByClassName('rangeslider');
 
     for (let slider of sliders)  {
         let output = document.getElementById(`${slider.id}output`)
-        slider.addEventListener("input", function  () {
+        slider.addEventListener('input', function  () {
             output.value = slider.value;
         });
     }
 
-    document.getElementById("lbutton").addEventListener("click", function () {
-        let text = document.getElementById("ltext").value
-        let bold = document.getElementById("lbold").checked
-        let italic = document.getElementById("litalic").checked
-        let fontSize = document.getElementById("lfontsizeoutput").value
+    document.getElementById('lbutton').addEventListener('click', function () {
+        let text = document.getElementById('ltext').value
+        let bold = document.getElementById('lbold').checked
+        let italic = document.getElementById('litalic').checked
+        let fontSize = document.getElementById('lfontsizeoutput').value
             
         if (text != '') {
             let url = new URL(window.location.href);
-            url.pathname = "/label";
-            url.searchParams.append("text", text);
-            url.searchParams.append("bold", bold);
-            url.searchParams.append("italic", italic);
-            url.searchParams.append("fontsize", fontSize);
+            url.pathname = '/label';
+            url.searchParams.append('text', text);
+            url.searchParams.append('bold', bold);
+            url.searchParams.append('italic', italic);
+            url.searchParams.append('fontsize', fontSize);
 
             console.log(url)
             
@@ -34,14 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    document.getElementById("ebutton").addEventListener("click", function () {
-        let text = removeFE0F(document.getElementById("etext").value)
+    document.getElementById('ebutton').addEventListener('click', function () {
+        let text = removeFE0F(document.getElementById('etext').value)
             
         if (text != '') {
             let url = new URL(window.location.href);
-            url.pathname = "/label";
-            url.searchParams.append("text", text);
-            url.searchParams.append("emoji", true);
+            url.pathname = '/label';
+            url.searchParams.append('text', text);
+            url.searchParams.append('emoji', true);
 
             console.log(url)
             
@@ -54,15 +54,15 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();  // Prevent the default form submission
     
         // Get the file input element
-        const fileInput = document.getElementById('file-input');
+        const fileInput = document.getElementById('ifile');
 
-        let autoRotate = document.getElementById("iautorotate").checked
+        let autoRotate = document.getElementById('iautorotate').checked
     
         // Get the selected file from the input element
         const file = fileInput.files[0];
     
         if (!file) {
-            alert("Please select a file before submitting.");
+            alert('Please select a file before submitting.');
             return;
         }
     
@@ -71,8 +71,8 @@ document.addEventListener('DOMContentLoaded', function() {
         formData.append('file', file);
 
         let url = new URL(window.location.href);
-        url.pathname = "/image";
-        url.searchParams.append("autorotate", autoRotate);
+        url.pathname = '/image';
+        url.searchParams.append('autorotate', autoRotate);
     
         try {
             // Send the file using fetch with a POST request
